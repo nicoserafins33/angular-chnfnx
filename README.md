d124c16d4cc2391231b6a0dcc1ccedeb3ef15750
nicoserafins33 <106156375+nicoserafins33@users.noreply.github.com>
2023-04-16 11:44:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-chnfnx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-chnfnx-jszqvl)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Configurable slider
+ */
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class SliderConfigurableExample {
+  disabled = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+}
+
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
